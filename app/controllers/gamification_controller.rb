f522a1cc8b8d55b8309be361a669c24ec2597afc
Thiago Felix <thiago.19.rafael@gmail.com>
2016-04-21 12:54:38 -0300

    Estrutura Gamification

---
+++ app/controllers/gamification_controller.rb
@@ -0,0 +1,23 @@
+class GamificationController < ApplicationController
+ 	def all
+		render json: Gamification.all
+	end
+	
+	def new
+	end
+	
+	def create
+	end
+	
+	def show
+	end
+	
+	def edit
+	end
+	
+	def update
+	end
+
+	def delete
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
   get 'users/:id' => 'user#show', format: :json
   get 'users/:id/update' => 'user#update', format: :json, as: :users_update
   delete 'users/:id' => 'user#delete', as: :users_delete
+	
+	get 'gamifications/all' => 'gamification#all', format: :json
+	get 'gamifications/new' => 'gamification#new', format: :json
+	post 'gamifications/create' => 'gamification#create'
+	get 'gamifications/:id' => 'gamification#show', format: :json
+	get 'gamification/:id/update' =>'gamification#update', format: :json, as: :gamifications_update
+	delete 'gamification/:id' => 'gamification#delete', as: :gamifications_delete
+
 end
