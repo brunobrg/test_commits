a27150b11572c0fbbee388e14d6855cfe780a755
Tulio Coelho <tulioopc@gmail.com>
2016-04-21 09:35:10 -0300

    Doing User controller with methods and routes

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,24 @@
+class UserController < ApplicationController
+
+  def all
+    render json: User.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
