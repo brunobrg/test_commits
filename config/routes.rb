827983479b59c6c0dd9e2c0639b7681fc9730093
Thiago Felix <thiago.19.rafael@gmail.com>
2016-04-21 15:01:17 -0300

    Ajuste de routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/all' => 'user#all', format: :json
   get 'users/new' => 'user#new'
-  get 'users/create' => 'user#create', format: :json
+  post 'users/create' => 'user#create', format: :json
   get 'users/:id' => 'user#show', format: :json
   get 'users/:id/update' => 'user#update', format: :json, as: :users_update
   delete 'users/:id' => 'user#delete', as: :users_delete
--- db/migrate/20160421145347_create_gamifications.rb
+++ db/migrate/20160421145347_create_gamifications.rb
@@ -1,6 +1,7 @@
 class CreateGamifications < ActiveRecord::Migration
   def change
     create_table :gamifications do |t|
+
 			
 			t.string :name
 			t.integer :experience_points
