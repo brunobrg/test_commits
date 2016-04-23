1eddc581a045d9ede6d147eeb1ef643d468715bb
Stefania <stefaniabezerra95@gmail.com>
2016-04-23 14:03:43 -0300

    crud_user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,20 +5,37 @@ class UserController < ApplicationController
   end
 
   def show
+
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(get_params)
+    if @user.save
+        render json: user
+      else
+        render action: :new
+end
+
   end
 
   def edit
+
   end
 
   def update
+
   end
 
   def delete
+
   end
+
+  private
+  def get_params
+        params.require(:user).permit(:nome,:email,:password)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
   acts_as :person
 
   has_secure_password
+
+  validates :nome, presence: true, length: { maximum: 50 }
+
+  before_save { self.email = email.downcase }
+  validates :email, presence: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
+  on: :create}, uniqueness: {case_sensitive: false}
+
+  validates :password, presence: true, length: { minimum: 6 }
+
 end
