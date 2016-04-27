2c475afcd8c00bdc0c921b413c2c29f5fe15a1b3
Matheus Oliveira <matheus_oliveira30@hotmail.com>
2016-04-27 14:43:21 -0300

    Quest validations updated

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -2,4 +2,8 @@ class Quest < ActiveRecord::Base
 	acts_as :gamification
 	validates_presence_of :name, :description, :experience_points
 	validates_numericality_of :experience_points
+
+	validades :name, lenght{minimum: 3}, uniquiness
+	validades :description, lenght{minimum: 10, maximum: 100}
+
 end
