f23aae03ac43391cbd09f1b1fc7d769debe9486a
Tulio Coelho <tulioopc@gmail.com>
2016-04-28 12:24:40 -0300

    removing email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,8 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
 
   before_save { self.email = email.downcase }
-  validates :email, presence: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
-  on: :create}, uniqueness: {case_sensitive: false}
+  validates :email, presence: true
 
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 6 }
 
 end
